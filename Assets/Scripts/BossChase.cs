d7e7393e51e78d94d1a9940b781e435746165dcd
hagerna <80783566+hagerna@users.noreply.github.com>
2022-02-24 21:45:43 +0100

    Fixed jump resetting, added boss chase script, included movement lock

---
+++ Assets/Scripts/BossChase.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossChase : MonoBehaviour
+{
+    public float startSpeed;
+    public float speedIncrement;
+    public float timeBeforeStart;
+    Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        StartCoroutine(startChase(timeBeforeStart));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    IEnumerator startChase(float wait)
+    {
+        yield return new WaitForSeconds(wait);
+        rb.velocity = new Vector2(startSpeed, 0);
+        while (true)
+        {
+            // start speed = 5, increment = 0.4, distance = 225 (double jumps: 4)
+            yield return new WaitForSeconds(1f);
+            startSpeed += speedIncrement;
+            rb.velocity = new Vector2(startSpeed, 0);
+        }
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,9 +6,12 @@ public class PlayerController : MonoBehaviour
 {
     public float speed;
     public float jumpForce;
+    bool checkJump;
     public ColorCycle cycle;
     Rigidbody2D rb2D;
     int jumpNumber;
+
+    public bool movementLocked;
 
     public GameObject projectile;
     public float projectileTimer;
@@ -25,51 +28,47 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector2 vel = rb2D.velocity;
-        float input = Input.GetAxis("Horizontal");
-        float xMovement = input * speed;
-        vel.x = xMovement;
+        if (!movementLocked)
+        {
+            Vector2 vel = rb2D.velocity;
+            float input = Input.GetAxis("Horizontal");
+            float xMovement = input * speed;
+            vel.x = xMovement;
 
-        // Trigger jump --> Double jump if cyan
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            vel = Jump(vel);
-        }
-
-        if (cycle.GetColor() == "yellow")
-        {
-            speed = 9f;
-        }
-        else
-        {
-            speed = 5f;
-        }
-
-        // Trigger color change
-        if (Input.GetKey(KeyCode.LeftShift))
-        {
-            cycle.Rotate();
-        }
-
-        if (Input.GetKey(KeyCode.RightShift) && cycle.GetColor() == "magenta")
-        {
-            ShootProjectile(input);
-        }
-
-        // Check jump reset
-        if (jumpNumber > 0)
-        {
-            RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, 0.51f);
-            if (hit.collider != null)
+            // Trigger jump --> Double jump if cyan
+            if (Input.GetKeyDown(KeyCode.Space))
             {
-                if (hit.collider.CompareTag("Platform"))
-                {
-                    jumpNumber = 0;
-                }
+                vel = Jump(vel);
             }
-        }
 
-        rb2D.velocity = vel;
+            if (cycle.GetColor() == "yellow")
+            {
+                speed = 9f;
+            }
+            else
+            {
+                speed = 5f;
+            }
+
+            // Trigger color change
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                cycle.Rotate();
+            }
+
+            if (Input.GetKey(KeyCode.RightShift) && cycle.GetColor() == "magenta")
+            {
+                ShootProjectile(input);
+            }
+
+            // Check jump reset
+            if (checkJump && jumpNumber > 0)
+            {
+                CheckJump();
+            }
+
+            rb2D.velocity = vel;
+        }
     }
 
 Vector2 Jump(Vector2 velocity)
@@ -78,13 +77,35 @@ Vector2 Jump(Vector2 velocity)
         {
             jumpNumber = 1;
             velocity.y = jumpForce;
+            checkJump = false;
         }
         else if (jumpNumber == 1 && cycle.GetColor() == "cyan")
         {
             jumpNumber = 2;
             velocity.y = jumpForce;
+            checkJump = false;
         }
+        checkJump = false;
+        StartCoroutine(JumpFrames());
         return velocity;
+    }
+
+    IEnumerator JumpFrames()
+    {
+        yield return new WaitForSeconds(0.05f);
+        checkJump = true;
+    }
+
+void CheckJump()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, 0.52f);
+        if (hit.collider != null)
+        {
+            if (hit.collider.CompareTag("Platform"))
+            {
+                jumpNumber = 0;
+            }
+        }
     }
 
 void ShootProjectile(float inputDirection)
@@ -96,21 +117,22 @@ void ShootProjectile(float inputDirection)
         canShoot = false;
         if (inputDirection >= 0)
         {
-            Instantiate(projectile, transform.position + Vector3.right, Quaternion.Euler(0,0,0));
+            Instantiate(projectile, transform.position + Vector3.right, Quaternion.Euler(0, 0, 0));
         }
         else
         {
-            Instantiate(projectile, transform.position + Vector3.left, Quaternion.Euler(0,0,180));
+            Instantiate(projectile, transform.position + Vector3.left, Quaternion.Euler(0, 0, 180));
         }
         Invoke(nameof(ResetShoot), projectileTimer);
     }
+
 
     void ResetShoot()
     {
         canShoot = true;
     }
 
-private void OnTriggerEnter2D(Collider2D other)
+    private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("CyanOrb"))
         {
@@ -127,5 +149,10 @@ private void OnTriggerEnter2D(Collider2D other)
             cycle.PickUpColor("magenta");
             Destroy(other.gameObject);
         }
+        if (other.CompareTag("Boss"))
+        {
+            Debug.Log(transform.position.x);
+        }
     }
+
 }
